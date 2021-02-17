36414ba3807525177014052e7561e389adff7f00
maria <maria@maria.dev>
2021-02-17 19:29:27 +0100

    Add base

---
+++ NeumorphicButtons/NeumorphicButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeumorphicButtonsApp.swift
+//  NeumorphicButtons
+//
+//  Created by Maria on 17/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct NeumorphicButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
